b957f99c0204ef5ccf6d7bc31d2d0b556446c935
Jakob Klepp <jakob.klepp@moonvision.io>
2021-10-22 15:52:24 +0200

    Get example working

--- shell.nix
+++ shell.nix
@@ -11,5 +11,13 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.libiconv
     pkgs.pkgconf
-  ];
+  ] ++ (
+    pkgs.lib.optionals pkgs.stdenv.isDarwin [
+      pkgs.darwin.apple_sdk.frameworks.Security
+      pkgs.darwin.apple_sdk.frameworks.CoreServices
+      pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+      pkgs.darwin.apple_sdk.frameworks.Foundation
+      pkgs.darwin.apple_sdk.frameworks.AppKit
+    ]
+  );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate env_logger;
+extern crate goji;
+
+use goji::{Credentials, Jira};
+use std::env;
+
+fn main() -> Result<(), &'static str> {
+    drop(env_logger::init());
+    if let (Ok(host), Ok(user), Ok(pass)) =
+    (
+        env::var("JIRA_HOST"),
+        env::var("JIRA_USER"),
+        env::var("JIRA_PASS"),
+    )
+    {
+        let query = env::args().nth(1).unwrap_or("".to_owned());
+
+        let jira = Jira::new(host, Credentials::Basic(user, pass)).unwrap();
+
+        match jira.search().iter(query, &Default::default()) {
+            Ok(results) => {
+                for issue in results {
+                    println!("{:#?}", issue);
+                }
+            }
+            Err(err) => panic!("{:#?}", err),
+        }
+        Ok(())
+    } else {
+        return Err("Env variables JIRA_HOST, JIRA_USER & JIRA_PASS need to be set!")
+    }
 }
