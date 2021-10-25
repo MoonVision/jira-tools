c0291c64e100cf006fd1a698af929bb9bdfc66cf
Jakob Klepp <jakob.klepp@moonvision.io>
2021-10-25 19:52:01 +0200

    Cleanup and fmt

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,17 @@
-use std::borrow::Borrow;
 extern crate env_logger;
 extern crate goji;
-use std::collections::{HashMap, HashSet};
+use goji::issues::IssueType;
 use goji::{Credentials, Jira};
-use goji::issues::{IssueType};
+use serde_derive::Deserialize;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::io::{Error, ErrorKind};
 use url::form_urlencoded;
-use serde_derive::Deserialize;
 
 fn create_error(error: &str) -> Result<(), Box<dyn std::error::Error>> {
-    Err(Box::new(Error::new(
-        ErrorKind::Other,
-        error,
-    )))
+    Err(Box::new(Error::new(ErrorKind::Other, error)))
 }
 
-/*
-{
-    "clauseNames": [
-        "issueLink"
-    ],
-    "custom": false,
-    "id": "issuelinks",
-    "key": "issuelinks",
-    "name": "Linked Issues",
-    "navigable": true,
-    "orderable": true,
-    "schema": {
-        "items": "issuelinks",
-        "system": "issuelinks",
-        "type": "array"
-    },
-    "searchable": true
-}
- */
 #[derive(Deserialize, Debug)]
 struct JiraField {
     custom: bool,
@@ -55,24 +32,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let resp = reqwest::Client::new()
             .get(field_endpoint)
             .basic_auth(jira_user.clone(), Some(jira_pass.clone()))
-            .send().await?
-            .json::<Vec<JiraField>>().await?;
-        // let story_point_fields: Vec<&JiraField> = resp.iter()
-        //     .filter(|jira_field| jira_field.name.to_lowercase().contains("story point"))
-        //     .collect();
-        //
-        // let mut story_point_field_ids: HashSet<String> = HashSet::new();
-        // resp.iter()
-        //     .filter(|jira_field| jira_field.name.to_lowercase().contains("story point"))
-        //     .for_each(|jira_field| {
-        //         story_point_field_ids.insert(jira_field.id.clone());
-        //     });
-
-        // story_point_fields.iter().for_each(|item| {
-        //     println!("Field name:\t{}", item.name);
-        //     println!("Field id:\t{}", item.id);
-        //     println!("-----");
-        // });
+            .send()
+            .await?
+            .json::<Vec<JiraField>>()
+            .await?;
 
         let mut story_point_fields: HashMap<String, &JiraField> = HashMap::new();
         resp.iter()
@@ -80,9 +43,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .for_each(|jira_field| {
                 story_point_fields.insert(jira_field.id.clone(), jira_field);
             });
-
-        // story_point_field_ids.into_iter().for_each(|item| println!("Field id:\t{}\n-----", item));
-        // println!("story_point_field_ids {:#?}", story_point_field_ids);
 
         let query = env::args().nth(1).unwrap_or("".to_owned());
 
@@ -92,7 +52,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Ok(results) => {
                 for issue in results {
                     println!("Key:\t\t\t{0}", issue.key);
-                    println!("Summary:\t\t{0}", issue.summary().unwrap_or("unset".to_owned()));
+                    println!(
+                        "Summary:\t\t{0}",
+                        issue.summary().unwrap_or("unset".to_owned())
+                    );
                     match issue.issue_type() {
                         None => {
                             println!("Type:\t\t\tUnknown!");
@@ -101,12 +64,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                             println!("Type:\t\t\t{0}", issue_type.name)
                         }
                     }
-                    // println!("- - -");
                     for (key, value) in issue.fields.iter() {
                         if story_point_fields.contains_key(key) {
                             println!("{}:\t{}", story_point_fields.get(key).unwrap().name, value);
                         }
-                        // issue.fields.get(key).map(|v| println!("{0} {1:#?}", key, value));
                     }
                     println!("-----");
                 }
