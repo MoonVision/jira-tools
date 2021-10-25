bbfe2506d10de50880f2695d2c279dd3fd5aeb28
Jakob Klepp <jakob.klepp@moonvision.io>
2021-10-25 16:36:27 +0200

    GET and "parse" field resource

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::borrow::Borrow;
-// extern crate env_logger;
-// extern crate goji;
+extern crate env_logger;
+extern crate goji;
 use std::collections::HashMap;
-// use goji::{Credentials, Jira};
-// use goji::issues::{IssueType};
+use goji::{Credentials, Jira};
+use goji::issues::{IssueType};
 use std::env;
 use std::io::{Error, ErrorKind};
 use url::form_urlencoded;
+use serde_derive::Deserialize;
 
 fn create_error(error: &str) -> Result<(), Box<dyn std::error::Error>> {
     Err(Box::new(Error::new(
@@ -15,62 +16,75 @@ fn create_error(error: &str) -> Result<(), Box<dyn std::error::Error>> {
     )))
 }
 
+/*
+{
+    "clauseNames": [
+        "issueLink"
+    ],
+    "custom": false,
+    "id": "issuelinks",
+    "key": "issuelinks",
+    "name": "Linked Issues",
+    "navigable": true,
+    "orderable": true,
+    "schema": {
+        "items": "issuelinks",
+        "system": "issuelinks",
+        "type": "array"
+    },
+    "searchable": true
+}
+ */
+#[derive(Deserialize, Debug)]
+struct JiraField {
+    custom: bool,
+    id: String,
+    key: String,
+    name: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // drop(env_logger::init());
+    drop(env_logger::init());
     if let (Ok(jira_host), Ok(jira_user), Ok(jira_pass)) = (
         env::var("JIRA_HOST"),
         env::var("JIRA_USER"),
         env::var("JIRA_PASS"),
     ) {
-        let sprint_endpoint = format!("{}/rest/api/3/search?jql=Sprint+%3D+84", jira_host);
-        let resp = reqwest::Client::new()
-            .get(sprint_endpoint)
-            .basic_auth(jira_user.clone(), Some(jira_pass.clone()))
-            .send().await?
-            .text().await?;
-        println!("{:#?}", resp);
-
         let field_endpoint = format!("{}/rest/api/3/field", jira_host);
         let resp = reqwest::Client::new()
             .get(field_endpoint)
             .basic_auth(jira_user.clone(), Some(jira_pass.clone()))
             .send().await?
-            .text().await?;
+            .json::<Vec<JiraField>>().await?;
         println!("{:#?}", resp);
-        // let query = env::args().nth(1).unwrap_or("".to_owned());
-        //
-        // let jira = Jira::new(host, Credentials::Basic(user, pass)).unwrap();
-        //
-        // match jira.search().iter(query, &Default::default()) {
-        //     Ok(results) => {
-        //         for issue in results {
-        //             // println!("{:#?}", issue);
-        //             match issue.project() {
-        //                 None => println!("Project unkown!"),
-        //                 Some(project) => println!("{}", project.key.as_str()),
-        //             };
-        //             println!("{0}\t{1}", issue.key, issue.summary().unwrap_or("unset".to_owned()));
-        //             // println!("{0}", issue.field("Story points").unwrap_or(Ok("unset".to_owned())).unwrap_or("error".to_owned()));
-        //             match issue.issue_type() {
-        //                 None => {
-        //                     println!("Unknown type!");
-        //                 }
-        //                 Some(issue_type) => {
-        //                     println!("{0}", issue_type.name)
-        //                 }
-        //             }
-        //             // println!("- - -");
-        //             for (key, value) in issue.fields.iter() {
-        //                 issue.fields.get(key).map(|v| println!("{0} {1:#?}", key, value));
-        //
-        //             }
-        //             println!("-----");
-        //         }
-        //     }
-        //     Err(err) => panic!("{:#?}", err),
-        // }
-        // Ok(())
+        let query = env::args().nth(1).unwrap_or("".to_owned());
+
+        let jira = Jira::new(jira_host, Credentials::Basic(jira_user, jira_pass)).unwrap();
+
+        match jira.search().iter(query, &Default::default()) {
+            Ok(results) => {
+                for issue in results {
+                    println!("Key:\t\t{0}", issue.key);
+                    println!("Summary:\t{0}", issue.summary().unwrap_or("unset".to_owned()));
+                    match issue.issue_type() {
+                        None => {
+                            println!("Type:\t\tUnknown!");
+                        }
+                        Some(issue_type) => {
+                            println!("Type:\t\t{0}", issue_type.name)
+                        }
+                    }
+                    // println!("- - -");
+                    // for (key, value) in issue.fields.iter() {
+                    //     issue.fields.get(key).map(|v| println!("{0} {1:#?}", key, value));
+                    //
+                    // }
+                    println!("-----");
+                }
+            }
+            Err(err) => panic!("{:#?}", err),
+        }
         Ok(())
     } else {
         create_error("Env variables JIRA_HOST, JIRA_USER & JIRA_PASS need to be set!")
