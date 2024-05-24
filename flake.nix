ac070821723e8acbdd40b0de856949fb961567ea
Jakob Klepp <jakob.klepp@moonvision.io>
2024-05-24 10:47:26 +0200

    nix fmt with alejandra

--- flake.nix
+++ flake.nix
@@ -5,38 +5,44 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
-          src =./.;
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-          ];
-        };
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            libiconv
-            openssl
-            rustc
-            rustfmt
-            pre-commit
-            rustPackages.clippy
-          ] ++
-          (lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.AppKit
-            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-            pkgs.darwin.apple_sdk.frameworks.CoreServices
-            pkgs.darwin.apple_sdk.frameworks.Foundation
-            pkgs.darwin.apple_sdk.frameworks.Security
-          ]);
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    naersk,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      naersk-lib = pkgs.callPackage naersk {};
+    in {
+      defaultPackage = naersk-lib.buildPackage {
+        src = ./.;
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+        ];
+      };
+      devShell = with pkgs;
+        mkShell {
+          buildInputs =
+            [
+              cargo
+              libiconv
+              openssl
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+            ]
+            ++ (lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.AppKit
+              pkgs.darwin.apple_sdk.frameworks.CoreFoundation
+              pkgs.darwin.apple_sdk.frameworks.CoreServices
+              pkgs.darwin.apple_sdk.frameworks.Foundation
+              pkgs.darwin.apple_sdk.frameworks.Security
+            ]);
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
-      });
+      formatter = pkgs.alejandra;
+    });
 }
