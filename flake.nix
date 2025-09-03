c0c0569f0357b228c50f6493dc6d99e19c544ef0
Jakob Klepp <jakob.klepp@moonvision.io>
2025-09-03 11:41:43 +0200

    Pass nixpkgs input to naersk and rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
